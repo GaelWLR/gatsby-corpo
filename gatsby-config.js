3b46a0d1ebcbb4d37ffcc6d48e4fa3ac57067b68
Gaël Weiler <gael.weiler@digitregroup.com>
2021-06-14 13:46:00 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteMetadata: {
+    title: "capi-gatsby",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: "o8n9dinz6b93",
+        spaceId: "",
+      },
+    },
+  ],
+};
